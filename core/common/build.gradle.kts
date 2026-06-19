604095ebf7600f9243e43796bc99ddcbd733ec07
sandro.mtchedlidze <sandro.mtchedlidze@space.ge>
2026-06-19 16:20:23 +0400

    added apiCall and apiResult

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-//    id("movieapp.android.library")
     alias(libs.plugins.movie.android.library)
     alias(libs.plugins.kotlin.serialization)
 }
@@ -9,4 +8,5 @@ android {
 
 dependencies {
     api(libs.bundles.coroutines)
+    implementation(libs.retrofit.core)
 }
---
+++ core/common/src/main/kotlin/com/space/common/network/ApiResult.kt
@@ -0,0 +1,9 @@
+package com.space.common.network
+sealed class ApiResult<out T> {
+    data object Loading : ApiResult<Nothing>()
+    data class Success<out T>(val data: T) : ApiResult<T>()
+    data class Error(
+        val message: String,
+        val throwable: Throwable? = null
+    ) : ApiResult<Nothing>()
+}
