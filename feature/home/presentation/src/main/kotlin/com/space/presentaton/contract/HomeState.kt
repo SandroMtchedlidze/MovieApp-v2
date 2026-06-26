1bd90ac5436c10a2323ee473d77d0c1d7757909b
sandro.mtchedlidze <sandro.mtchedlidze@space.ge>
2026-06-26 14:36:49 +0400

    fix homestate

--- feature/home/presentation/src/main/kotlin/com/space/presentaton/contract/HomeState.kt
+++ feature/home/presentation/src/main/kotlin/com/space/presentaton/contract/HomeState.kt
@@ -1,10 +1,6 @@
 package com.space.presentaton.contract
 
-import androidx.paging.PagingData
-import com.space.ui.component.MovieCardUiModel
-
 data class HomeState(
- //   val movies: PagingData<MovieCardUiModel> = PagingData.empty(),
     val isLoading: Boolean = false,
     val errorMessage: String? = null
 )
