ec74b6d274456644e18547de9f6e330573294170
sandro.mtchedlidze <sandro.mtchedlidze@space.ge>
2026-07-29 17:26:08 +0400

    removed un used code

--- feature/home/data/src/main/kotlin/com/space/data/remote/mapper/MovieMapper.kt
+++ feature/home/data/src/main/kotlin/com/space/data/remote/mapper/MovieMapper.kt
@@ -4,7 +4,7 @@ import com.space.data.remote.dto.movie.MovieDto
 import com.space.domain.model.MovieResponse
 
 class MovieMapper {
-    fun mapToDomain(dto: MovieDto, genreMap: Map<Int, String>): MovieResponse {
+    fun mapToDomain(dto: MovieDto): MovieResponse {
         return MovieResponse(
             id = dto.id,
             title = dto.title,
